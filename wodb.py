09037785151f9d2743cefea7830a6e548c742d01
Evan Weinberg <weinbergmath@Evans-MacBook-Pro.local>
2015-08-16 14:18:34 +0800

    addMoreComments
    More comments added to explain the steps of the program.

--- wodb.py
+++ wodb.py
@@ -6,30 +6,32 @@ os.makedirs('wodbimages',exist_ok=True) # Creates a directory for the images bei
 
 photolinks = [] #Array for storing the links for all of the photos
 
-res = requests.get('http://wodb.ca/shapes.html')
+res = requests.get('http://wodb.ca/shapes.html') #This makes a request to download the entire web page from the web address.
 
-res.raise_for_status()
+res.raise_for_status() #If there are any errors, this will stop the program.
 
-html = bs4.BeautifulSoup(res.text,"html.parser") #Use BS to create a searchable HTML object
+html = bs4.BeautifulSoup(res.text,"html.parser") #Use Beautiful Soup to create a searchable HTML object
 
-imageDivs = html.select(".displayed") #Store all of the div tags containing displayed images
+imageDivs = html.select(".displayed") #Store all of the div tags containing images with the class "displayed"
 
 size = len(imageDivs) #Find the number of images in the array
 
-for image in imageDivs:
+for image in imageDivs: #For each of the images on the page...
     address = "http://wodb.ca/"+image.get('src')
-    photolinks.append(address)
+    photolinks.append(address) #...store the URL for the image in the photolinks array. 
 
 
 
 
-for photolink in photolinks[:]:
+for photolink in photolinks[:]: #For each of the links for the images on the page:
     print(photolink)
-    res = requests.get(photolink)
+    res = requests.get(photolink) #Request the image be sent from the server
     res.raise_for_status()
-    imageFile = open(os.path.join('wodbimages',os.path.basename(photolink)),'wb')
+    imageFile = open(os.path.join('wodbimages',os.path.basename(photolink)),'wb') #Save the image data to a file on the local computer
     for chunk in res.iter_content(100000):
         imageFile.write(chunk)
-    imageFile.close()
+    imageFile.close() 
  
 
+#Now you have all of the shape images in the directory wodbimages.
+#Can you change this to download the other categories of WODB images?
