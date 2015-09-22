55a202e3360e1978708138a1bcdaf496af03eb25
Evan Weinberg <weinbergmath@gealgerobophysiculus-central.local>
2015-09-22 21:24:30 +0800

    updateSept22
    Changed some of the text to better describe what happens in the program.

--- wodb.py
+++ wodb.py
@@ -1,37 +1,36 @@
 
 import requests,bs4,os
-os.makedirs('wodbimages',exist_ok=True) # Creates a directory for the images being downloaded
+os.makedirs('wodbimages',exist_ok=True) # Creates a directory for the images being downloaded called 'wodbimages'
 
 
 
-photolinks = [] #Array for storing the links for all of the photos
+photolinks = [] #Create a python list for storing the links for all of the photos
 
-res = requests.get('http://wodb.ca/shapes.html') #This makes a request to download the entire web page from the web address.
+res = requests.get('http://wodb.ca/shapes.html')
 
-res.raise_for_status() #If there are any errors, this will stop the program.
+res.raise_for_status()
 
 html = bs4.BeautifulSoup(res.text,"html.parser") #Use Beautiful Soup to create a searchable HTML object
 
-imageDivs = html.select(".displayed") #Store all of the div tags containing images with the class "displayed"
+imageDivs = html.select(".displayed") #Store all of the HTML tags containing images that have class = "displayed"
 
-size = len(imageDivs) #Find the number of images in the array
+size = len(imageDivs) #Find the number of images in the page
 
-for image in imageDivs: #For each of the images on the page...
-    address = "http://wodb.ca/"+image.get('src')
-    photolinks.append(address) #...store the URL for the image in the photolinks array. 
+for image in imageDivs:
+    address = "http://wodb.ca/"+image.get('src') #Get the address of the image...
+    print(address) 
+    photolinks.append(address) #...and store it in our list.
 
 
 
-
-for photolink in photolinks[:]: #For each of the links for the images on the page:
+#Now for each address in our list, download the file onto our local computer.
+for photolink in photolinks[:]:
     print(photolink)
-    res = requests.get(photolink) #Request the image be sent from the server
+    res = requests.get(photolink)
     res.raise_for_status()
-    imageFile = open(os.path.join('wodbimages',os.path.basename(photolink)),'wb') #Save the image data to a file on the local computer
+    imageFile = open(os.path.join('wodbimages',os.path.basename(photolink)),'wb')
     for chunk in res.iter_content(100000):
         imageFile.write(chunk)
-    imageFile.close() 
+    imageFile.close()
  
 
-#Now you have all of the shape images in the directory wodbimages.
-#Can you change this to download the other categories of WODB images?
